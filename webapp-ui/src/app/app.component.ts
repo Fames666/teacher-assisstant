a54322991fd3d0462c99e16383f101674d3da951
Fames666 <ekaterinazubkova63@gmail.com>
2023-02-19 20:30:26 +0300

    subtask/FE-Header

--- webapp-ui/src/app/app.component.ts
+++ webapp-ui/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '[element=root]',
+  selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
