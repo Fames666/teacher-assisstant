d55ee041e98559225bec738ac53e401f0dbbd9ff
Fames666 <92324932+Fames666@users.noreply.github.com>
2023-02-20 15:26:12 +0300

    subtask/FE-Breadcrumbs

--- webapp-ui/src/app/app.module.ts
+++ webapp-ui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
