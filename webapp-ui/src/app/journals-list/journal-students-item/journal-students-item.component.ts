dab3fade745b28adc18e72e7b8ac46380da6a387
Fames666 <ekaterinazubkova63@gmail.com>
2023-05-08 23:03:48 +0300

    subtask/FE-Journal-page

--- webapp-ui/src/app/journals-list/journal-students-item/journal-students-item.component.ts
+++ webapp-ui/src/app/journals-list/journal-students-item/journal-students-item.component.ts
@@ -8,9 +8,20 @@ import { Input } from '@angular/core';
 })
 export class JournalStudentsItemComponent {
 
+  // TODO: replace with value from BE
+  private static readonly ONLINE_STATUS = 'Онлайн';
+
   @Input() public id?: string;
   @Input() public fullName?: string;
   @Input() public lastOnline?: string;
   @Input() public marker?: string;
   @Input() public remarksAmount?: number;
+
+  public resolveMarkerType(): string {
+    if (this.remarksAmount === 0) {
+      return this.lastOnline === JournalStudentsItemComponent.ONLINE_STATUS
+          ? 'online' : 'offline';
+    }
+    return 'remark';
+  }
 }
